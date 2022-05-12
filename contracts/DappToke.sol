9ff2baa7dce7ae26c221b86334c57349743fb61d
charlesmariga <charlesmariga37@gmail.com>
2022-05-12 10:34:15 +0300

    Created the ERC20 token smart contract

--- contracts/DappToke.sol
+++ contracts/DappToke.sol
@@ -3,9 +3,14 @@ pragma solidity >=0.4.2 < 0.9.0;
 
 contract DappToken {
 
+  string public name  = "Dapp Token";
+  string public symbol = "DAPP";
+  string public standard = "Dapp Token v1.0";
   uint256 public totalSupply;
+  mapping(address => uint256) public balanceOf; 
 
-  constructor() {
-    totalSupply = 1000000;
+  constructor(uint256 _initialSupply) {
+    balanceOf[msg.sender] = _initialSupply;
+    totalSupply = _initialSupply;
   }
 }
--- test/DappToken.js
+++ test/DappToken.js
@@ -1,13 +1,33 @@
 const DappToken = artifacts.require("DappToken.sol");
 
 contract("DappToken", function (accounts) {
-  it("sets the total supply upon deployment", async function () {
+  it("initializes the contract with the correct values", async function () {
+    const tokenInstance = await DappToken.deployed();
+    const name = await tokenInstance.name();
+    assert.equal(name, "Dapp Token", "has the correct name");
+    const symbol = await tokenInstance.symbol();
+    assert.equal(symbol, "DAPP", "has the correct symbol");
+    const standard = await tokenInstance.standard();
+    assert.equal(standard, "Dapp Token v1.0", "has the correct standard");
+  });
+
+  it("allocatoes initial supply upon deployment", async function () {
+    // Check that the total supply vairable has been set to the total initial
+    //  coin offering
     const tokenInstance = await DappToken.deployed();
     const totalSupply = await tokenInstance.totalSupply();
     assert.equal(
       totalSupply.toNumber(),
       1000000,
       "sets the total supply to 1,000,000"
+    );
+
+    // Check that the admin balance is set to the total initial coin offering
+    const adminBalance = await tokenInstance.balanceOf(accounts[0]);
+    assert.equal(
+      adminBalance.toNumber(),
+      1000000,
+      "it allocates the initial supply to the admin account"
     );
   });
 });
